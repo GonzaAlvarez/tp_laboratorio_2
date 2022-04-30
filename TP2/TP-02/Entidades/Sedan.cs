df1300a72626c7c84303c089d40aca506daf6add
GonzaAlvarez <gonzaalvarezz@hotmail.com>
2022-04-30 14:17:30 -0300

    Se sube el TP2, Cambio el nombre de la carpeta TP1

---
+++ TP2/TP-02/Entidades/Sedan.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        #region "Enumerados"
+        public enum ETipo 
+        {
+           CuatroPuertas, CincoPuertas 
+        }
+        #endregion
+
+        #region "Atributos"
+        ETipo tipo;
+        #endregion
+
+        #region "Constructores"
+        /// <summary>
+        /// Por defecto, TIPO será CuatroPuertas
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color) : base(chasis, marca, color)
+        {
+            this.tipo = ETipo.CuatroPuertas;
+        }
+
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo) :this(marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
+        #endregion
+
+        #region "Propiedades"
+        /// <summary>
+        /// Sedan son 'Mediano'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+            set
+            {
+                this.Tamanio = ETamanio.Mediano;
+            }
+        }
+        #endregion
+
+        #region "Metodos"
+        /// <summary>
+        /// Muestra los datos de Sedan
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO: {this.Tamanio} TIPO: {this.tipo}");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
